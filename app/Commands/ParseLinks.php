40b2dbe0f969bf736af6b58a009e3712c4e81834
Aurielle <gmvasek@php-info.cz>
2013-02-21 21:02:30 +0100

    Whitespace

--- app/Commands/ParseLinks.php
+++ app/Commands/ParseLinks.php
@@ -109,26 +109,26 @@ class ParseLinks extends Nette\Object
 		//
 		// This more complex version comes from http://stackoverflow.com/questions/5830387/how-to-find-all-youtube-video-ids-in-a-string-using-a-regex/5831191#5831191
 		$regex = '~
-        # Match non-linked youtube URL in the wild. (Rev:20111012)
-        https?://         # Required scheme. Either http or https.
-        (?:[0-9A-Z-]+\.)? # Optional subdomain.
-        (?:               # Group host alternatives.
-          youtu\.be/      # Either youtu.be,
-        | youtube\.com    # or youtube.com followed by
-          \S*             # Allow anything up to VIDEO_ID,
-          [^\w\-\s]       # but char before ID is non-ID char.
-        )                 # End host alternatives.
-        ([\w\-]{11})      # $1: VIDEO_ID is exactly 11 chars.
-        (?=[^\w\-]|$)     # Assert next char is non-ID or EOS.
-        (?!               # Assert URL is not pre-linked.
-          [?=&+%\w]*      # Allow URL (query) remainder.
-          (?:             # Group pre-linked alternatives.
-            [\'"][^<>]*>  # Either inside a start tag,
-          | </a>          # or inside <a> element text contents.
-          )               # End recognized pre-linked alts.
-        )                 # End negative lookahead assertion.
-        [?=&+%\w-]*        # Consume any URL (query) remainder.
-        ~ix';
+			# Match non-linked youtube URL in the wild. (Rev:20111012)
+			https?://         # Required scheme. Either http or https.
+			(?:[0-9A-Z-]+\.)? # Optional subdomain.
+			(?:               # Group host alternatives.
+			  youtu\.be/      # Either youtu.be,
+			| youtube\.com    # or youtube.com followed by
+			  \S*             # Allow anything up to VIDEO_ID,
+			  [^\w\-\s]       # but char before ID is non-ID char.
+			)                 # End host alternatives.
+			([\w\-]{11})      # $1: VIDEO_ID is exactly 11 chars.
+			(?=[^\w\-]|$)     # Assert next char is non-ID or EOS.
+			(?!               # Assert URL is not pre-linked.
+			  [?=&+%\w]*      # Allow URL (query) remainder.
+			  (?:             # Group pre-linked alternatives.
+			    [\'"][^<>]*>  # Either inside a start tag,
+			  | </a>          # or inside <a> element text contents.
+			  )               # End recognized pre-linked alts.
+			)                 # End negative lookahead assertion.
+			[?=&+%\w-]*        # Consume any URL (query) remainder.
+			~ix';
 		$matches = Nette\Utils\Strings::match($link, $regex);
 		$vid = $matches[1];
 
