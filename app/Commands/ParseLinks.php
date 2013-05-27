b20913298d54eadd2ae0485c81dc70c55317e351
Aurielle <gmvasek@php-info.cz>
2013-05-27 16:12:56 +0200

    Replaced link matching regex and fixed multiline titles [Closes #11]

--- app/Commands/ParseLinks.php
+++ app/Commands/ParseLinks.php
@@ -106,11 +106,30 @@ class ParseLinks extends Nette\Object implements Events\Subscriber
 
 	private function matchLinks($data)
 	{
-		// Comes from Nette\Utils\Validators::isUrl()
-		$alpha = "a-z\x80-\xFF";
-		$domain = "[0-9$alpha](?:[-0-9$alpha]{0,61}[0-9$alpha])?";
-		$topDomain = "[$alpha][-0-9$alpha]{0,17}[$alpha]";
-		return Nette\Utils\Strings::matchAll($data, "(https?://((?:$domain\\.)*$topDomain|\\d{1,3}\.\\d{1,3}\.\\d{1,3}\.\\d{1,3})(:\\d{1,5})?(/\\S*)?)i");
+		// From: http://daringfireball.net/2010/07/improved_regex_for_matching_urls
+		$regex = '~
+			(?xi)
+			\b
+			(                       # Capture 1: entire matched URL
+			  (?:
+			    https?://               # http or https protocol
+			    |                       #   or
+			    www\d{0,3}[.]           # "www.", "www1.", "www2." … "www999."
+			    |                           #   or
+			    [a-z0-9.\-]+[.][a-z]{2,4}/  # looks like domain name followed by a slash
+			  )
+			  (?:                       # One or more:
+			    [^\s()<>]+                  # Run of non-space, non-()<>
+			    |                           #   or
+			    \(([^\s()<>]+|(\([^\s()<>]+\)))*\)  # balanced parens, up to 2 levels
+			  )+
+			  (?:                       # End with:
+			    \(([^\s()<>]+|(\([^\s()<>]+\)))*\)  # balanced parens, up to 2 levels
+			    |                               #   or
+			    [^\s`!()\[\]{};:\'".,<>?«»“”‘’]        # not a space or one of these punct chars
+			  )
+			)~ix';
+		return Nette\Utils\Strings::matchAll($data, $regex);
 	}
 
 
@@ -224,6 +243,8 @@ class ParseLinks extends Nette\Object implements Events\Subscriber
 		    return FALSE;
 		}
 
-		return sprintf('[Web] %s', html_entity_decode(trim($title[1]), $flags, 'UTF-8'));	// title can contain any entity
+		$t = trim($title[1]);
+		$t = str_replace(array("\r\n", "\r", "\n"), array("\n", "\n", ' '), $t);
+		return sprintf('[Web] %s', html_entity_decode($t, $flags, 'UTF-8'));	// title can contain any entity
 	}
 }
